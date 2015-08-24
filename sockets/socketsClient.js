eee296763d572c95482dfb80c58877997aa6744a
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-08-24 11:24:30 -0500

    Fixed sockets demo
    Because of the bad design the states of individual instances might get out of sync.

--- sockets/socketsClient.js
+++ sockets/socketsClient.js
@@ -3,40 +3,39 @@ import {PAUSE, RESTART} from 'flux-lumines/src/game/actions.js';
 import {PAUSED} from 'flux-lumines/src/game/gameStates.js';
 import styles from '../styles.css';
 
+const socketUrl = 'ws://localhost:9091';
 const lumines = new Lumines(document.getElementById('lumines'));
-const ws = new WebSocket('ws://localhost:9091');
-ws.onmessage = event => {
-    // First message we receive from the server is the current game state
-    lumines.setState(JSON.parse(event.data));
-    ws.onmessage = () => {};
-};
 
 document.getElementById('broadcast').onclick = () => {
-    if (ws.readyState != WebSocket.OPEN) {
-        alert('The socket connection has not been established! Is the server running?');
-        return;
-    }
-    removeButtons();
+    const ws = new WebSocket(socketUrl);
+    ws.onopen = () => {
+        removeButtons();
 
-    lumines.register(action => {
-        ws.send(JSON.stringify(action));
-    });
-    lumines.start();
+        lumines.register(action => {
+            ws.send(JSON.stringify(action));
+        });
+        lumines.start();
+    };
 };
 
 document.getElementById('listen').onclick = () => {
-    if (ws.readyState != WebSocket.OPEN) {
-        alert('The socket connection has not been established! Is the server running?');
-        return;
-    }
-    removeButtons();
+    const ws = new WebSocket(socketUrl);
+    ws.onopen = () => {
+        removeButtons();
 
-    ws.onmessage = event => {
-        const {action, payload} = JSON.parse(event.data);
-        lumines.dispatch(action, payload);
-        lumines.render();
+        ws.onmessage = event => {
+            // First message we receive from the server is the current game state
+            lumines.setState(JSON.parse(event.data));
+
+            ws.onmessage = event => {
+                const {action, payload} = JSON.parse(event.data);
+                lumines.dispatch(action, payload);
+                lumines.render();
+            };
+
+            lumines.render();
+        };
     };
-    lumines.render();
 };
 
 function removeButtons() {
